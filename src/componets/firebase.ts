e54c5afdc430734cebc74dbd8694bf8a30370934
Abdullah9842 <tuerme0@gmail.com>
2025-02-17 00:45:20 +0300

    Update firebase.ts

--- src/componets/firebase.ts
+++ src/componets/firebase.ts
@@ -23,13 +23,7 @@ import { getAuth } from "firebase/auth";
 
 // إعداد Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCGx4Zjd2X9LoMGvNf42x9YKAyZ1xjSrGo",
-  authDomain: "jadeksa-69140.firebaseapp.com",
-  projectId: "jadeksa-69140",
-  storageBucket: "jadeksa-69140.firebasestorage.app",
-  messagingSenderId: "597164029013",
-  appId: "1:597164029013:web:72272a88da2a33aa5190dc",
-  measurementId: "G-6PJL6V2KJG"
+
 };
 
 // تهيئة Firebase
