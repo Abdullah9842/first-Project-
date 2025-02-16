b3b8dc1d1db259cf66f8c3742d0f0fd0adca3452
macbook <macbook@macbooks-MacBook-Pro.local>
2025-02-17 01:25:33 +0300

    Your descriptive commit message

--- src/componets/firebase.ts
+++ src/componets/firebase.ts
@@ -3,28 +3,7 @@
 
 
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
 
-// // إعداد Firebase
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-// };
-
-// // تهيئة Firebase
-// const app = initializeApp(firebaseConfig);
-
-// // تصدير auth للاستخدام في المكونات الأخرى
-// export const auth = getAuth(app);
-
-// // تصدير app (اختياري)
-// export default app;import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // إعداد Firebase باستخدام المتغيرات البيئية
